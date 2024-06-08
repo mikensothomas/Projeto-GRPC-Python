173318a7ffede098bfb1b8c56840a4344250a795
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-06-08 10:48:51 -0300

    commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mikensonthomas/Área de trabalho/Projeto GRPC Python/venv"
+set -gx VIRTUAL_ENV "/home/mikensonthomas/Downloads/Projeto-GRPC-Python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
